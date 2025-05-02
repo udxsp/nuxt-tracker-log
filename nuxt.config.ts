f8db135c2b0b4c0f5661a64ebabe1e6a250bf8d7
udxsp <udxsp@outlook.com>
2025-05-02 17:40:12 +0200

    chore: lint fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ['@nuxt/eslint'],
   eslint: {
     config: {
       standalone: false,
     },
   },
-});
+})
